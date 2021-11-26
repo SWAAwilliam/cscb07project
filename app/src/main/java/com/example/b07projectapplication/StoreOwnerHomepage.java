561ca3c85a5e6624895b446810125bab6fcc1b3e
William <slicer7667@gmail.com>
2021-11-25 19:24:12 -0500

    Added an View Products Screen (for Store Owners)

--- app/src/main/java/com/example/b07projectapplication/StoreOwnerHomepage.java
+++ app/src/main/java/com/example/b07projectapplication/StoreOwnerHomepage.java
@@ -2,7 +2,10 @@ package com.example.b07projectapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StoreOwnerHomepage extends AppCompatActivity {
 
@@ -11,5 +14,11 @@ public class StoreOwnerHomepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_owner_homepage);
         getSupportActionBar().hide();
+    }
+
+    public void openProductPage(View view){
+        Intent intent = new Intent(this, StoreOwner_ViewProducts.class);
+        Button button = (Button) findViewById(R.id.btnProducts);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/b07projectapplication/StoreOwner_ViewProducts.java
@@ -0,0 +1,17 @@
+package com.example.b07projectapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreOwner_ViewProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_owner_view_products);
+        getSupportActionBar().hide();
+    }
+
+
+}
