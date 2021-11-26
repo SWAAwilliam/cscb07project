e05c71d49fd9233707594cad3a377457f39b6de9
William <slicer7667@gmail.com>
2021-11-26 00:47:27 -0500

    Added Functionality to Write Users to Database

--- app/src/main/java/com/example/b07projectapplication/CustomerAccountActivity.java
+++ app/src/main/java/com/example/b07projectapplication/CustomerAccountActivity.java
@@ -10,17 +10,21 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.b07projectapplication.ui.login.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CustomerAccountActivity extends AppCompatActivity {
 
     private EditText input_email;
     private EditText input_password;
-    private EditText input_fullname;
+    private EditText input_firstName;
+    private EditText input_lastName;
     private Button btn_create;
     private FirebaseAuth auth;
     private FirebaseUser user;
@@ -33,7 +37,8 @@ public class CustomerAccountActivity extends AppCompatActivity {
 
         input_email = findViewById(R.id.editEmail);
         input_password = findViewById(R.id.editPassword);
-        input_fullname = findViewById(R.id.editFullName);
+        input_firstName = findViewById(R.id.editFirstName);
+        input_lastName = findViewById(R.id.editLastName);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
@@ -44,43 +49,46 @@ public class CustomerAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 authentication();
-
             }
         });
     }
 
-    /*public void createCAccount() {
-        //Change the purpose of this button to creating an account when clicked
-        Intent intent = new Intent(this, CustomerAccountActivity.class);
-        Button button = (Button) findViewById(R.id.c_account);
-        startActivity(intent);
-
-    }
-
-     */
     private void authentication(){
         String email = input_email.getText().toString();
-        String fullname = input_fullname.getText().toString();
+        String firstName = input_firstName.getText().toString();
+        String lastName = input_lastName.getText().toString();
         String password = input_password.getText().toString();
 
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    createUser( firstName, lastName, user.getUid() );
                     sendUser();
-                    Toast.makeText(CustomerAccountActivity.this, "Created New Account Successfully", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(CustomerAccountActivity.this, "Created New Account Successfully, Please Log In", Toast.LENGTH_SHORT).show();
 
                 }
             }
         });
 
+    }
+
+    private void createUser(String firstName, String lastName, String userUID){
+        //NEED TO MODIFY THIS FOR WHEN WE CREATE A OWNER/CUSTOMER SWITCH
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        Person person = new Person();
+        person.setUserUID( userUID );
+        person.setFirstName( firstName );
+        person.setLastName( lastName );
+
+        ref.child("users").child( person.getUserUID() ).setValue(person);
 
     }
+
     private void sendUser(){
-        Intent intent = new Intent(CustomerAccountActivity.this, NewAccountActivity.class);
+        Intent intent = new Intent(CustomerAccountActivity.this, LoginActivity.class);
         intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TASK|intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
-
 
 }
