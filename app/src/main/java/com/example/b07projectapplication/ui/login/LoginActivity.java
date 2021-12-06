8f41ee96cf649d2508b1b4ae0581d17fdae8327f
William <slicer7667@gmail.com>
2021-12-06 01:01:51 -0500

    Removed Unnecessary Code

--- app/src/main/java/com/example/b07projectapplication/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/b07projectapplication/ui/login/LoginActivity.java
@@ -92,25 +92,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -223,12 +204,6 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = "Welcome! " + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-    }
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
